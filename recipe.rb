ad5a09f9e2104a15ef206fddb6e573816ecf6d87
Mike Adeleke <madeleke13@gmail.com>
2014-02-16 22:38:13 -0500

    any questions?

--- recipe.rb
+++ recipe.rb
@@ -15,3 +15,23 @@ puts "\nMethod"
 recipe.method.each_with_index do |step, index|
 	puts "#{index+1}. #{step}"
 end
+
+train = {}
+train[:current_city] = "Chicago"
+train[:number_of_engines] = 4
+train[:number_of_cars] = 3
+train[:caboose] = 1
+
+Passenger = Struct.new(:train, :method)
+
+passenger = Passenger.new( {current_city: "Chicago", number_of_engines: 4, number_of_cars: 3, caboose: 1}, ["The Polar Express"] )
+
+puts "train"
+passenger.train.each do |key, value|
+  puts "* #{key}: #{value}"
+end
+
+puts "\nMethod"
+passenger.method.each_with_index do |step, index|
+  puts "#{index+1}. #{step}"
+end
